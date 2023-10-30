5835e01fc51522cf58ecd427004d59f7404116a4
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-10-30 14:34:04 +0300

    Resubmitted assignment

--- index.js
+++ index.js
@@ -1,49 +1,36 @@
-// Add your code here
-// Functions
-const url = 'http://localhost:3000/users';
+const userName = "Jim";
+const userEmail = "jim@jim.com";
+// console.log(userEmail)
 
-function submitData(userName, userEmail){
+function submitData(userName, userEmail) {
+    const apiUrl = 'http://localhost:3000/users'
 
-    let newUser = {
-        name: userName,
-        email: userEmail
-    }
-
-    const options = {
+    fetch(apiUrl, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
-            Accept: "application/json"
+            Accept: "application/json",
         },
-        body: JSON.stringify(newUser)
-    };
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then(response => response.json())
+    .then(user => {
+        const userID = user.id
+        const h1 = document.createElement('h1')
+        h1.textContent = userID
+        document.body.appendChild(h1)
+        // console.log(userID)        
+    })
+    .catch(error => () => {
+        const h1 = document.createElement('h1')
+        history.textContent = 'Unauthorized Access'
+        document.body.appendChild(h1) 
+    })
 
-    fetch(url, options) 
-    .then(response => {
-        if(response.ok){
-            return response.json();
-        } else {
-        throw new Error('Response was not OK!');
-        }
-    })
-    .then(data => {
-        console.log(data)
-        let div = document.createElement('div');
-        let li = document.createElement('li')
-        li.innerHTML = `
-            <p>Name: ${data.name}</p> <br> 
-            <p>Email: ${data.email}</p>
-        `
-        div.appendChild(li)
-        document.body.appendChild(div)
-    })
-    .catch(error => {
-        console.error('There was an error!', error)
-    })
+
 }
 
-// function deleteUser(Id){
-
-// }
-
-submitData('Melky', 'melky@gmail.com')
+submitData(userName, userEmail)
